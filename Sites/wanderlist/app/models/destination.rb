e70fd61d9c34124acb7a167fc0e51627033ceae2
Kelsey Fecho <kfecho30@gmail.com>
2018-04-22 18:16:30 -0400

    Added associations to models

--- Sites/wanderlist/app/models/destination.rb
+++ Sites/wanderlist/app/models/destination.rb
@@ -1,2 +1,4 @@
 class Destination < ActiveRecord::Base
+  has_many :trip_destinations
+  has_many :trips, through: :trip_destinations
 end
--- Sites/wanderlist/app/models/trip.rb
+++ Sites/wanderlist/app/models/trip.rb
@@ -1,2 +1,5 @@
 class Trip < ActiveRecord::Base
+  belongs_to :user
+  has_many :trip_destinations
+  has_many :destinations, through: :trip_destinations
 end
