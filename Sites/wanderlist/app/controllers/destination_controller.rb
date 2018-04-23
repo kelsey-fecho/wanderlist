5b528d9bb2472c7432f1d5b43313330dd83dfc10
Kelsey Fecho <kfecho30@gmail.com>
2018-04-23 14:03:04 -0400

    Added ability to create new Destinations

--- Sites/wanderlist/app/controllers/destination_controller.rb
+++ Sites/wanderlist/app/controllers/destination_controller.rb
@@ -4,6 +4,21 @@ class DestinationController < ApplicationController
     erb :'destinations/index'
   end
 
+  get '/destinations/new' do
+    erb :'destinations/new'
+  end
+
+  post '/destinations/new' do
+    if Destination.all.find{|d| d.name == params[:name]}
+      #add a flash message for failure
+      redirect '/destinations/new'
+    else
+      @dest = Destination.create(params)
+      #add a flash message for success
+      redirect "/destinations/#{@dest.id}"
+    end
+  end
+
   get '/destinations/:id' do
     @dest = Destination.find(params[:id])
     erb :'destinations/show'
