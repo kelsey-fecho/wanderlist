ef7cd8e1b9a8f5562184e461f6de80bfd838d00c
Kelsey Fecho <kfecho30@gmail.com>
2018-04-23 10:36:57 -0400

    Added post controller for login

--- Sites/wanderlist/app/controllers/user_controller.rb
+++ Sites/wanderlist/app/controllers/user_controller.rb
@@ -1,3 +1,14 @@
 
 class UserController < ApplicationController
+  
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/destinations'
+    else
+      redirect '/'
+    end
+  end
+
 end
