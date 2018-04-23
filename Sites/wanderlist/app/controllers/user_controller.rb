5038a5be4bca1568f7d4f297194fd27824fb13fc
Kelsey Fecho <kfecho30@gmail.com>
2018-04-23 11:28:59 -0400

    Added user signup functionality

--- Sites/wanderlist/app/controllers/user_controller.rb
+++ Sites/wanderlist/app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 
 class UserController < ApplicationController
-  
+
   post '/login' do
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
@@ -8,6 +8,20 @@ class UserController < ApplicationController
       redirect '/destinations'
     else
       redirect '/'
+    end
+  end
+
+  get '/signup' do
+    erb :'users/new'
+  end
+
+  post '/signup' do
+    user = User.new(:email => params[:email], :username => params[:username], :password => params[:password])
+    if user.save && user.username != "" && user.email != ""
+      session[:user_id] = user.id
+      redirect '/destinations'
+    else
+      redirect '/signup'
     end
   end
 
